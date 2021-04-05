a3ae403508b7f5624ce17c23968c16b7b22f36f5
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-05 02:32:08 -0700

    fix

--- objects/score.py
+++ objects/score.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class LeaderboardScore:
-    username: str
-    date: int
-    place: int # pos on leaderboard (change later)
+    username: str = ""
+    date: int = 0
+    place: int = 0# pos on leaderboard (change later)
--- objects/user.py
+++ objects/user.py
@@ -2,31 +2,31 @@ from dataclasses import dataclass
 
 @dataclass
 class Account:
-    username: str
-    email: str
-    account_id: int
-    register_date: int
-    privileges: int
-    stars: int
-    demons: int
-    icon: int
-    color1: int
-    color2: int
-    icon_type: int
-    coins: int
-    user_coins: int
-    ship: int
-    ball: int
-    wave: int
-    robot: int
-    glow: bool
-    cp: int
-    diamonds: int
-    orbs: int
-    spider: int
-    explosion: int
-    banned: bool
-    youtube: str
-    twitter: str
-    twitch: str
-    pp: int
+    username: str = ""
+    email: str = ""
+    account_id: int = 0
+    register_date: int = 0
+    privileges: int = 0
+    stars: int = 0
+    demons: int = 0
+    icon: int = 0
+    color1: int = 0
+    color2: int = 0
+    icon_type: int = 0
+    coins: int = 0
+    user_coins: int = 0
+    ship: int = 0
+    ball: int = 0
+    wave: int = 0
+    robot: int = 0
+    glow: bool = False
+    cp: int = 0
+    diamonds: int = 0
+    orbs: int = 0
+    spider: int = 0
+    explosion: int = 0
+    banned: bool = False
+    youtube: str = ""
+    twitter: str = ""
+    twitch: str = ""
+    pp: int = 0
