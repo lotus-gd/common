ccf6c4c4b5e0428f62d0b118a0b5c176a39b923a
spookybear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-09 12:03:44 -0700

    remove refresh leaderboards as it is no longer needed

--- helpers/leaderboardhelper.py
+++ helpers/leaderboardhelper.py
@@ -5,10 +5,3 @@ async def get_top_100():
     db = await get_db()
     users = await db.fetchall("SELECT username, pp FROM accounts ORDER BY pp DESC;")
     return users
-    
-async def refresh_leaderboards():
-    db = await get_db()
-    users = list(await db.fetchall("SELECT pp, user_id from accounts;"))
-    users.sort(reverse=True)
-    for i, u in enumerate(users, start=1):
-        await db.execute("INSERT INTO `leaderboard` (`position`, `user_id`) VALUES (%s, %s);", (i, u[1]))
