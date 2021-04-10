9f1ab298da751ad685107b1e50d57108afd222c8
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-09 22:18:53 -0700

    remove incorrect import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from common.objects.account import Account
 from common.objects.privilege import PrivilegeGroup
 
-from common.db.mysql import MySQLPool, get_db
-
 from common.helpers import leaderboardhelper
 from common.helpers import userhelper
