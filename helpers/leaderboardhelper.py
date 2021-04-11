709f65a9e1217ec71bd2a5c3ac175288afebb4a2
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-04-11 19:04:00 +0100

    we need to redo this entire helper

--- helpers/leaderboardhelper.py
+++ helpers/leaderboardhelper.py
@@ -1,5 +1,5 @@
 from common.globals import sql
 
 async def get_top_100():
-    users = await sql.fetchall("SELECT username, pp FROM accounts ORDER BY pp DESC;")
+    users = await sql.fetchall("SELECT username, pp FROM users ORDER BY pp DESC LIMIT 100")
     return users
