80cf31c3318bbcd2ff6ca068c46628e11e710a69
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-05-30 11:56:14 -0700

    add database level helper

--- helpers/levelhelper.py
+++ helpers/levelhelper.py
@@ -1,7 +1,32 @@
-
+import gd
+from common.objects.level import Level
 
 async def get_level():
     pass
 
 async def get_level_by_name():
     pass
+
+async def database_level():
+    """
+    Database level from robs servers using gd.py
+    """
+    client = gd.Client()
+    levels = await client.search_levels(filters=gd.Filters(demon_difficulty=gd.DemonDifficulty.EXTREME_DEMON), pages=range(34))
+    for l in levels:
+        if isinstance(l.difficulty, gd.DemonDifficulty):
+            demon_difficulty = l.difficulty.value
+            difficulty = 10
+        else:
+            difficulty = l.difficulty
+            demon_difficulty = 0
+            
+        if not l.password:
+            password = 0
+        else:
+            password = l.password
+        
+        lvl = await Level.database(l.id, l.name, l.description, l.version, l.downloads,
+                       l.rating, l.score, l.creator.name, l.song.id, difficulty,
+                       demon_difficulty, password, l.stars, l.coins, l.uploaded_timestamp,
+                       l.length.value, l.object_count)
